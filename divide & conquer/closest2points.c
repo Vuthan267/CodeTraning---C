9ed20385577c324a1c1a9a9d65c46120e04226c2
Ho Vu Thanh <hovuthanh12345@gmail.com>
2026-08-26 12:11:12 +0700

    Fix some formatting

--- divide & conquer/closest2points.c
+++ divide & conquer/closest2points.c
@@ -9,57 +9,61 @@
 #include <math.h>
 
 int compareX(const void *a, const void *b) {
-    double x1 = ((Point*)a)->x;
-    double x2 = ((Point*)b)->x;
-    return (x1 > x2) - (x1 < x2);
+	double x1 = ((Point *) a)->x;
+	double x2 = ((Point *) b)->x;
+	return (x1 > x2) - (x1 < x2);
 }
 
 int compareY(const void *a, const void *b) {
-    double y1 = ((Point*)a)->y;
-    double y2 = ((Point*)b)->y;
-    return (y1 > y2) - (y1 < y2);
+	double y1 = ((Point *) a)->y;
+	double y2 = ((Point *) b)->y;
+	return (y1 > y2) - (y1 < y2);
 }
 
 double dist(Point a, Point b) {
-    return sqrt(pow(a.x - b.x, 2) + pow(a.y - b.y, 2));
+	return sqrt(pow(a.x - b.x, 2) + pow(a.y - b.y, 2));
 }
 
 double bruteDist(Point arr[], int low, int high) {
-    if (low == high) return DBL_MAX;
-    return dist(arr[low], arr[high]);
+	if (low == high) return DBL_MAX;
+	return dist(arr[low], arr[high]);
 }
 
 double stripClosest(Point strip[], int size, double minSides) {
-    double minClosest = minSides;
-    qsort(strip, size, sizeof(Point), compareY);
+	double minClosest = minSides;
+	qsort(strip, size, sizeof(Point), compareY);
 
-    for (int i = 0; i < size; i++)
-        for (int j = i + 1; j < size && (strip[j].y - strip[i].y < minClosest); j++)
-            if (dist(strip[i], strip[j]) < minClosest)
-                minClosest = dist(strip[i], strip[j]);
+	for (int i = 0; i < size; i++)
+		for (int j = i + 1; j < size && (strip[j].y - strip[i].y < minClosest); j++)
+			if (dist(strip[i], strip[j]) < minClosest)
+				minClosest = dist(strip[i], strip[j]);
 
-    return minClosest;
+	return minClosest;
 }
 
 double closestSolve(Point arr[], int low, int high) {
-    if (high - low <= 1) return bruteDist(arr, low, high);
+	if (high - low <= 1) return bruteDist(arr, low, high);
 
-    int mid = low + (high - low) / 2;
-    double minDistLeft = closestSolve(arr, low, mid);
-    double minDistRight = closestSolve(arr, mid + 1, high);
-    double minSides = fmin(minDistLeft, minDistRight);
-    Point midPoint = arr[mid];
+	int mid = low + (high - low) / 2;
+	Point midPoint = arr[mid];
 
-    Point strip[high+1];
-    int j = 0;
-    for (int i = 0; i < high+1; i++)
-        if (fabs(arr[i].x - midPoint.x) < minSides)
-            strip[j++] = arr[i];
+	double minDistLeft = closestSolve(arr, low, mid);
+	double minDistRight = closestSolve(arr, mid + 1, high);
+	double minSides = fmin(minDistLeft, minDistRight);
 
-    return fmin(stripClosest(strip, j, minSides), minSides);
+	Point strip[high + 1];
+	int j = 0;
+	for (int i = 0; i < high + 1; i++) {
+		if (fabs(arr[i].x - midPoint.x) < minSides) {
+			strip[j] = arr[i];
+			j++;
+		}
+	}
+
+	return fmin(stripClosest(strip, j, minSides), minSides);
 }
 
 double closest(Point arr[], int low, int high) {
-    qsort(arr, high + 1, sizeof(Point), compareX);
-    return closestSolve(arr, low, high);
+	qsort(arr, high + 1, sizeof(Point), compareX);
+	return closestSolve(arr, low, high);
 }
--- divide & conquer/mergesort.c
+++ divide & conquer/mergesort.c
@@ -10,23 +10,24 @@ void merge(int arr[], const int l, const int m, const int h) {
     for (int i = 0; i < n1; i++) L[i] = arr[l + i];
     for (int i = 0; i < n2; i++) R[i] = arr[m + i + 1];
 
-    int i = 0, j = 0, k = l;
+    int i = 0;
+	int j = 0;
+	int k = l;
     while (i < n1 && j < n2) {
         if (L[i] < R[j]) {
-            arr[k] = L[i];
-            i++;
+	        arr[k] = L[i];
+        	i++;
+        } else {
+	        arr[k] = R[j];
+        	j++;
         }
-        else {
-            arr[k] = R[j];
-            j++;
-        }
-        k++;
+    	k++;
     }
 
     while (i < n1) {
         arr[k] = L[i];
-        i++;
-        k++;
+    	k++;
+    	i++;
     }
 
     while (j < n2) {
